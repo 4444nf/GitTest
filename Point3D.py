dce17579f3b4a8bc67667e04cc80f0c1ea28eb45
4444nf <fanninegyesi@gmail.com>
2023-04-15 17:43:20 +0200

    fixed the class Point3D and Point2D

--- Point3D.py
+++ Point3D.py
@@ -1,6 +1,5 @@
 from Point2D import Point2D
 
-
 class Point3D(Point2D):
     def __init__(self, x: float, y: float, z: float):
         self._z = z
@@ -12,12 +11,13 @@ class Point3D(Point2D):
     def set_z(self, z):
         self._z = z
 
-    def get_xyz(self, x, y, z):  #TODO fix this, there is not need for parameters in getter
+    def get_xyz(self) :
         return [self._x, self._y, self._z]
+
     def set_xyz(self, x, y, z):
         self._x = x
         self._y = y
         self._z = z
 
-    def to_str(self) -> str: #TODO fix this string
-        return f"Point3D [{super().to_str()}", z={self._z}]"
+    def to_str(self) -> str:
+        return f"Point3D [ {super().to_str() }"  f" z = {self._z } ]"
